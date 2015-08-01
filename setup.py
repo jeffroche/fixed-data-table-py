ac13af3c6037b0bdfa52e324aa796c489e0dd540
Jeff Roche <jeff.roche@gmail.com>
2015-08-01 18:30:30 -0400

    v0.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='fixeddatatable',
-    version='0.0.0',
+    version='0.0.1',
     description='Utilities for interfacing with FixedDataTable.js',
     long_description=long_description,
     url='https://github.com/jeffroche/fixed-datatable-py',
